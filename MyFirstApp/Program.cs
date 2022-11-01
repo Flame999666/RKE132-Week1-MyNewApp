1e2e753209774dfaa080da3a3229eedb17c0a05f
Artur Bal <artur.balosev@tktk.ee>
2022-11-01 14:11:35 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:" );
+string userName = Console.ReadLine();
+Console.WriteLine("Hello, " + userName + "!");
+Console.WriteLine("How are you " + userName + "?");
+Console.ReadLine();
